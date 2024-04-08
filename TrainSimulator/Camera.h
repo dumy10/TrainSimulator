e238a5c8aec863027b9fd3f3b96c10b5f6224e81
dumy10 <146924332+dumy10@users.noreply.github.com>
2024-04-08 22:35:30 +0300

    Camera class

---
+++ TrainSimulator/Camera.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <GL/glew.h>
+
+#include <GLM.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+
+class Camera
+{
+public:
+	const static enum ECameraMovementType
+	{
+		UNKNOWN,
+		FORWARD,
+		BACKWARD,
+		LEFT,
+		RIGHT,
+		UP,
+		DOWN
+	};
+
+	Camera(const int width, const int height, const glm::vec3& position);
+	void Set(const int width, const int height, const glm::vec3& position);
+	void Reset(const int width, const int height);
+	void Reshape(int windowWidth, int windowHeight);
+	const glm::mat4 GetViewMatrix() const;
+	const glm::vec3 GetPosition() const;
+	const glm::mat4 GetProjectionMatrix() const;
+	void ProcessKeyboard(ECameraMovementType direction, float deltaTime);
+	void MouseControl(float xPos, float yPos);
+	void ProcessMouseScroll(float yOffset);
+
+protected:
+	const float cameraSpeedFactor = 10.0f;
+	const float mouseSensitivity = 0.2f;
+
+	// Perspective properties
+	float zNear;
+	float zFar;
+	float FoVy;
+	int width;
+	int height;
+	bool isPerspective;
+
+	glm::vec3 position;
+	glm::vec3 forward;
+	glm::vec3 right;
+	glm::vec3 up;
+	glm::vec3 worldUp;
+
+	// Euler Angles
+	float yaw;
+	float pitch;
+
+	bool bFirstMouseMove = true;
+	float lastX = 0.f, lastY = 0.f;
+
+private:
+	// Default camera values
+	const float zNEAR = 0.1f;
+	const float zFAR = 500.f;
+	const float YAW = -90.0f;
+	const float PITCH = 0.0f;
+	const float FOV = 45.0f;
+	glm::vec3 startPosition;
+
+private:
+	void ProcessMouseMovement(float xOffset, float yOffset, bool constrainPitch = true);
+	void UpdateCameraVectors();
+};
